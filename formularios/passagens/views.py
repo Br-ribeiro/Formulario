8b0884dd465bc0234b4ec9233f8c7be796d0f485
Bernardo Ribeiro <bernardosr25@gmail.com>
2021-10-29 14:56:03 -0300

    feito a criação da pagina para visualização

--- formularios/passagens/views.py
+++ formularios/passagens/views.py
@@ -8,3 +8,9 @@ def index(request):
         'form':form
     }
     return render(request, 'index.html', contexto)
+
+def revisao_consulta(request):
+    if request.method == 'POST':
+        form = PassagemForms(request.POST)
+        contexto = {'form':form}
+        return render(request, 'minha_consulta.html', contexto)
